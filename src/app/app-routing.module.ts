5546f30489ae9b26298be91f187a51f4031b8fc8
fodorpapbalazsdev <fodorpapbalazsdev@gmail.com>
2021-01-26 21:56:27 +0100

    navbar design, create core components and create basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './core/components/home/home.component';
+import {AboutComponent} from './core/components/about/about.component';
+import {CharactersComponent} from './core/components/characters/characters.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'characters', component: CharactersComponent},
+  {path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/components/home/home.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { CharactersComponent } from './core/components/characters/characters.component';
+import { AboutComponent } from './core/components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    CharactersComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
