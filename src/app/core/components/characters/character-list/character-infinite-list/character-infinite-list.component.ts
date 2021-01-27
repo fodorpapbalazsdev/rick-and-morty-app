fbca540724250b96ab7d8827a599363812bf80eb
fodorpapbalazsdev <fodorpapbalazsdev@gmail.com>
2021-01-27 18:27:52 +0100

    folder restructure
    move character lists below to the characters folder

--- src/app/core/components/characters/character-list/character-infinite-list/character-infinite-list.component.ts
+++ src/app/core/components/characters/character-list/character-infinite-list/character-infinite-list.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Character} from '../../../models/character.model';
+import {Character} from '../../../../models/character.model';
 
 @Component({
   selector: 'app-character-infinite-list',
